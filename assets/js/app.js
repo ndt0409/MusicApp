2d318a24ace9716d63648411654cf8625dc63aae
NDT <vivucenter@gmail.com>
2024-09-13 20:58:40 +0700

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -645,12 +645,12 @@ const app = {
 
     songsUk: [
         {
-            name: "Senorita",
-            singer: "Shawn Mendes",
+            name: "Lucid Dream",
+            singer: "Anonymous",
             path: "SongUk_1.mp3",
             image: "SongUk_1.jpg",
             duration: 191,
-            lyrics: `I love it when you call me señorita|
+            lyrics: `NDT`/*`I love it when you call me señorita|
             I wish I could pretend I didn't need ya|
             But every touch is ooh, la-la-la|
             It's true, la-la-la|
@@ -708,7 +708,7 @@ const app = {
             For ya|<br>|
             Ooh, I should be running|
             Ooh, you keep me coming for ya|
-            `
+            `*/
         },
         {
             name: "I'm a mess",
