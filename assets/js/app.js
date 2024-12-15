70ad0879f6a6ddd8f8e69f379f5a2064360742f4
NDT <vivucenter@gmail.com>
2024-12-16 00:13:48 +0700

    update aihongnhan

--- assets/js/app.js
+++ assets/js/app.js
@@ -266,77 +266,12 @@ const app = {
             Hẹn nhau nhé, hẹn nhau kiếp sau tìm nhau...|`
         },
         {
-            name: "FRIENDS",
-            singer: "Anne Marie",
+            name: "Ải Hồng Nhan",
+            singer: "Dung Hoàng",
             path: "Song_7.mp3",
             image: "Song_7.jpg",
             duration: 231,
-            lyrics: `Ooh ooh, ooh ooh|
-            Ooh ooh, ooh ooh|<br>|
-            You say you love me, I say you crazy|
-            We're nothing more than friends|
-            You're not my lover, more like a brother|
-            I known you since we were like ten, yeah|<br>|
-            Don't mess it up, talking that shit|
-            Only gonna push me away, that's it|
-            When you say you love me, that make me crazy|
-            Here we go again|<br>|
-            Don't go look at me with that look in your eye|
-            You really ain't going away without a fight|
-            You can't be reasoned with, I'm done being polite|
-            I've told you one, two, three, four, five, six thousand times|<br>|
-            Haven't I made it obvious?|
-            Haven't I made it clear?|
-            Want me to spell it out for you?|
-            F-R-I-E-N-D-S|
-            Haven't I made it obvious?|
-            Haven't I made it clear?|
-            Want me to spell it out for you?|
-            F-R-I-E-N-D-S|
-            F-R-I-E-N-D-S|<br>|
-            Have you got no shame? You looking insane|
-            Turning up at my door|
-            It's two in the morning, the rain is pouring|
-            Haven't we been here before?|<br>|
-            Don't mess it up, talking that shit|
-            Only gonna push me away, that's it|
-            Have you got no shame? You looking insane|
-            Here we go again|<br>|
-            So don't go look at me with that look in your eye|
-            You really ain't going away without a fight|
-            You can't be reasoned with, I'm done being polite|
-            I've told you one, two, three, four, five, six thousand times|<br>|
-            Haven't I made it obvious? (Haven't I made it?)|
-            Haven't I made it clear? (Haven't I made it clear?)|
-            Want me to spell it out for you?|
-            F-R-I-E-N-D-S|
-            Haven't I made it obvious?|
-            Haven't I made it clear? (Haven't I?)|
-            Want me to spell it out for you? (To spell it out for you?)|
-            F-R-I-E-N-D-S|
-            F-R-I-E-N-D-S|<br>|
-            F-R-I-E-N-D-S|
-            That's how you f- spell "friends"|
-            F-R-I-E-N-D-S|
-            Get that shit inside your head|
-            No, no, yeah, uh, ah|
-            F-R-I-E-N-D-S|
-            We're just friends|<br>|
-            So don't go look at me with that look in your eye|
-            You really ain't going nowhere without a fight|
-            You can't be reasoned with, I'm done being polite|
-            I've told you one, two, three, four, five, six thousand times|<br>|
-            Haven't I made it obvious? (Have I not made it obvious?)|
-            Haven't I made it clear? (I made it very clear)|
-            Want me to spell it out for you? (Yo)|
-            F-R-I-EN-D-S (I said F-R-I-E-N-D-S)|
-            Haven't I made it obvious? (I made it very obvious)|
-            Haven't I made it clear? (I made it very clear)|
-            Want me to spell it out for you?|
-            F-R-I-E-N-D-S|
-            F-R-I-E-N-D-S|<br>|
-            Mmm, ooh ooh, ooh ooh|
-            Ah, ah-oh, ah|`
+            lyrics: `Nhân dân thi trăm vạn người...|`
         },
         {
             name: "Người Thương Em Cả Đời Em Xua Đuổi",
